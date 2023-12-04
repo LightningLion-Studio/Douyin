fad1dff82aea9a3bf44b80edfc7d7e1f02e11cf8
Zero的Mac <1203970284@qq.com>
2023-12-04 08:33:28 +0800

    feat: first commit

--- src/closer.ts
+++ src/closer.ts
@@ -5,8 +5,10 @@ export class Closer {
     try {
       const waiter = await page.waitForSelector(selector);
       await waiter.click();
+      console.log(`关闭${selector}弹窗成功`);
       this.begin(page, selector);
     } catch (error) {
+      console.log(`关闭${selector}弹窗失败 没有检测到弹窗`);
       this.begin(page, selector);
     }
   }
